ebba894230db8aab9832eefa28f74cca6752f62c
Filza Tanweer <filzatanweer25@gmail.com>
2025-09-30 12:28:29 +0500

    Create Q1(part a).cpp

---
+++ Lab6/Q1(part a).cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+    int data;
+    Node* prev;
+    Node* next;
+
+public:
+    Node(int val) {
+        data = val;
+        prev = NULL;
+        next = NULL;
+    }
+
+    friend class DoublyLinkedList;
+};
+
+class DoublyLinkedList {
+    Node* head;
+
+public:
+    DoublyLinkedList() {
+        head = NULL;
+    }
+    void insertAtPos(int val, int pos) {
+        Node* newNode = new Node(val);
+
+        if (pos == 1) { 
+            newNode->next = head;
+            if (head != NULL) head->prev = newNode;
+            head = newNode;
+            return;
+        }
+
+        Node* temp = head;
+        for (int i = 1; i < pos - 1 && temp != NULL; i++) {
+            temp = temp->next;
+        }
+
+        if (temp == NULL) {
+            cout << "Position out of range!" << endl;
+            delete newNode;
+            return;
+        }
+
+        newNode->next = temp->next;
+        newNode->prev = temp;
+
+        if (temp->next != NULL) {
+            temp->next->prev = newNode;
+        }
+
+        temp->next = newNode;
+    }
+    
+    void deleteAtPos(int pos) {
+        if (head == NULL) {
+            cout << "List is empty!" << endl;
+            return;
+        }
+
+        Node* temp = head;
+
+        if (pos == 1) { 
+            head = head->next;
+            if (head != NULL) head->prev = NULL;
+            delete temp;
+            return;
+        }
+
+        for (int i = 1; i < pos && temp != NULL; i++) {
+            temp = temp->next;
+        }
+
+        if (temp == NULL) {
+            cout << "Position out of range!" << endl;
+            return;
+        }
+
+        if (temp->prev != NULL) 
+		temp->prev->next = temp->next;
+        if (temp->next != NULL) 
+		temp->next->prev = temp->prev;
+
+        delete temp;
+    }
+
+    void display() {
+        Node* temp = head;
+        while (temp != NULL) {
+            cout << temp->data << " <-> ";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+};
+
+int main() {
+    DoublyLinkedList dll;
+
+    dll.insertAtPos(10, 1); 
+    dll.insertAtPos(20, 2); 
+    dll.insertAtPos(25, 2); 
+    dll.display();
+
+    dll.deleteAtPos(2);     
+    dll.display();
+
+    dll.insertAtPos(40, 3); 
+    dll.display();
+
+    return 0;
+}
+
+
